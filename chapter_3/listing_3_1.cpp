752f27841ea05a73f77e08fd090a14896fdec197
Vejmon <vemundhell@gmail.com>
2026-03-11 20:40:13 +0100

    add dereferencer

--- chapter_3/listing_3_1.cpp
+++ chapter_3/listing_3_1.cpp
@@ -1,8 +1,10 @@
 ﻿#include <cstdio>
 
 int main() {
-  int gettysburg{};
+  int gettysburg{3};
   printf("gettysburg: %d\n", gettysburg);
   int* gettysburg_address = &gettysburg;
   printf("&gettysburg: %p\n", gettysburg_address);
+  int gettysb_dereferenced{*gettysburg_address};
+  printf("gettysb_dereferenced: %d\n", gettysb_dereferenced);
 }
