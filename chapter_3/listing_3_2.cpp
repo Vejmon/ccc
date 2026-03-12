fa7d0e82713b04cc8f7869f5915164d71bf82e5c
Vejmon <vemundhell@gmail.com>
2026-03-12 14:26:46 +0100

    implemented improved_print and added some checks to 3_2

--- chapter_3/listing_3_2.cpp
+++ chapter_3/listing_3_2.cpp
@@ -5,7 +5,10 @@ int main() {
   int* gettysburg_address = &gettysburg;
   printf("Value at gettysburg_address: %d\n", *gettysburg_address);
   printf("Gettysburg Address: %p\n", gettysburg_address);
-  *gettysburg_address = 17325;
+  *gettysburg_address = 17326;
+  printf("Value at gettysburg_address: %d\n", *gettysburg_address);
+  printf("Gettysburg Address: %p\n", gettysburg_address);
+  gettysburg = 123;
   printf("Value at gettysburg_address: %d\n", *gettysburg_address);
   printf("Gettysburg Address: %p\n", gettysburg_address);
 }
--- chapter_3/listing_3_5.cpp
+++ chapter_3/listing_3_5.cpp
@@ -1,4 +1,8 @@
 #include <cstdio>
+#include <iostream>
+#include <vector>
+
+using namespace std;
 
 struct College {
   char name[256];
@@ -10,7 +14,15 @@ void print_names(College* colleges, size_t n_colleges) {
   }
 }
 
+void improved_print_names(vector<College>& colleges) {
+  for (const auto& college : colleges) {
+    cout << college.name << " College, improved" << endl;
+  }
+}
+
 int main() {
   College oxford[] = { "Magdalen", "Nuffield", "Kellogg" };
+  vector<College> colleges = { {"Magdalen"}, {"Nuffield"}, {"Kellogg"} };
   print_names(oxford, sizeof(oxford) / sizeof(College));
+  improved_print_names(colleges);
 }
