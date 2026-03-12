3f4ab0fafde2819156fb4e740966779cbfaaddee
Vejmon <vemundhell@gmail.com>
2026-03-12 18:34:25 +0100

    implemented exercises in chapter 3

--- chapter_3/listing_3_10.cpp
+++ chapter_3/listing_3_10.cpp
@@ -2,9 +2,16 @@
 
 struct Element {
   Element* next{};
+  Element* previous{};
   void insert_after(Element* new_element) {
-    new_element->next = next;
-    next = new_element;
+    new_element->previous = this;
+    this->next = new_element;
+  }
+  void insert_before(Element* new_element)
+  {
+    new_element->next = this;
+    new_element->previous = previous;
+    previous = new_element;
   }
   char prefix[2];
   short operating_number;
@@ -15,16 +22,24 @@ int main() {
   trooper1.prefix[0] = 'T';
   trooper1.prefix[1] = 'K';
   trooper1.operating_number = 421;
-  trooper1.insert_after(&trooper2);
+//  trooper1.insert_after(&trooper2);
   trooper2.prefix[0] = 'F';
   trooper2.prefix[1] = 'N';
   trooper2.operating_number = 2187;
-  trooper2.insert_after(&trooper3);
+//  trooper2.insert_after(&trooper3);
   trooper3.prefix[0] = 'L';
   trooper3.prefix[1] = 'S';
   trooper3.operating_number = 005;
+  trooper3.insert_before(&trooper2);
+  trooper2.insert_before(&trooper1);
 
-  for(Element* cursor = &trooper1; cursor; cursor = cursor->next) {
+
+  printf("front-to-back\n");
+  for(const Element* cursor = &trooper1; cursor; cursor = cursor->next) {
+    printf("Storm Trooper %c%c-%d\n", cursor->prefix[0], cursor->prefix[1], cursor->operating_number);
+  }
+  printf("back-to-front\n");
+  for(const Element* cursor = &trooper3; cursor; cursor = cursor->previous) {
     printf("Storm Trooper %c%c-%d\n", cursor->prefix[0], cursor->prefix[1], cursor->operating_number);
   }
 }
--- chapter_3/listing_3_6.cpp
+++ chapter_3/listing_3_6.cpp
@@ -1,4 +1,28 @@
 #include <cstdio>
+#include <cstring>
+#include <stdexcept>
+
+bool check_bounds(const char* array, const int index)
+{
+  if (const size_t array_len = strlen(array); index < 0 || index >= array_len)
+  {
+    throw std::invalid_argument(index < 0 ? "Neative index is not allowed" : "Index out of bounds");
+  }
+  return true;
+}
+
+
+char read_from(const char *array, const int index)
+{
+  check_bounds(array, index);
+  return array[index];
+}
+
+void write_to(char *array, const int index, const char value)
+{
+  check_bounds(array, index);
+  array[index] = value;
+}
 
 int main() {
   char lower[] = "abc?e";
@@ -8,10 +32,11 @@ int main() {
   lower[3] = 'd'; // lower now contains a b c d e \0
   upper_ptr[3] = 'D'; // upper now contains A B C D E \0
 
-  char letter_d = lower[3]; // letter_d equals 'd'
-  char letter_D = upper_ptr[3]; // letter_D equals 'D'
+  char letter_d = read_from(lower, 3); // letter_d equals 'd'
+  char letter_D = read_from(upper, 3); // letter_D equals 'D'
 
   printf("lower: %s\nupper: %s", lower, upper);
 
-  lower[7] = 'g'; // Super bad. You must never do this.
+  write_to(lower, 7, 'g'); // Super bad. You must never do this.
+
 }
